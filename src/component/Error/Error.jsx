7a3a724a1921747a2b3a37c9480b2baf08145027
mo-bahaa2 <medobsb2@gmail.com>
2025-04-03 13:36:54 +0200

    edit6 commit

--- src/component/Error/Error.jsx
+++ src/component/Error/Error.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import img from '/public/rafiki.png';
 export default function Error(){
   return <>
-    <div className="flex flex-col items-center justify-center min-h-screen bg-white-100 p-6">
+    <div className="flex items-center justify-center min-h-96 py-14">
       <div className="bg-white rounded-2xl p-8 max-w-md w-full text-center">
-        <h1 className="text-5xl font-bold text-red-500 mb-4">404</h1>
-        <h2 className="text-2xl font-semibold text-gray-800 mb-4">Page Not Found</h2>
-        <p className="text-gray-600 mb-6">
-          Oops! The page you're looking for doesn't exist.
-        </p>
+        <img src={img} className='w-full pb-8' alt="" />
         <Link to="/" className="bg-[#FFBE33] text-white py-3 px-6 rounded-lg hover:bg-yellow-400 transition">
           Back to Home
         </Link>
