04259f122f3561cf11874cf3c3ecc3138b46f0d4
YousefHatem4 <youssefhatem242004@gmail.com>
2025-04-04 23:26:52 +0200

    update about

--- src/component/About/About.jsx
+++ src/component/About/About.jsx
@@ -16,12 +16,12 @@ export default function About() {
           <p className='text-base sm:text-lg mb-6 leading-relaxed'>
             There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.
           </p>
-          <button
+          {/* <button
             type="button"
             className="text-white bg-[#FFBE33] hover:bg-[#d99e1f] cursor-pointer rounded-full px-8 sm:px-10 py-2 sm:py-3 focus:ring-4 focus:outline-none font-medium text-sm sm:text-base transition duration-300"
           >
             Read More
-          </button>
+          </button> */}
         </div>
       </div>
     </div>
